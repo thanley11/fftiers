6f956a31909ab6d2ee784446ae54b75680dd4426
Tom Hanley <thanley11@gmail.com>
2014-07-14 14:12:56 +0000

    Fixed centering 'content-box' updated populate script, updated sortables

--- models.py
+++ models.py
@@ -8,12 +8,14 @@ class Player(db.Model):
     player_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     position = db.Column(db.Enum('QB', 'RB', 'WR', 'TE', 'DEF', 'K', name='player_position'))
-    #url
-    #bye
+    bye = db.Column(db.Integer)
+    url = db.Column(db.String(64))
 
-    def __init__(self, name, position):
+    def __init__(self, name, position, bye, url):
         self.name     = name
         self.position = position
+        self.bye      = bye
+        self.url      = url
 
     def __repr__(self):
         return '<Player %r>' % (self.name)
--- populate.py
+++ populate.py
@@ -9,16 +9,20 @@
 #Need to add url for player and bye
 
 def populate():
-    player_1 = add_player('Tom Brady','QB')
-    player_2 = add_player('Peyton Manning','QB')
-    player_3 = add_player('Lesean McCoy','RB')
-    player_4 = add_player('Calvin Johnson','WR')
-    player_5 = add_player('Jimmy Graham','TE')
-    player_6 = add_player('Seattle','DEF')
+    player_1 = add_player('Tom Brady','QB',8,'http://www.espn.com')
+    player_2 = add_player('Peyton Manning','QB',3,'http://www.espn.com')
+    player_3 = add_player('Lesean McCoy','RB',8,'http://www.espn.com')
+    player_4 = add_player('Calvin Johnson','WR',8,'http://www.espn.com')
+    player_5 = add_player('Jimmy Graham','TE',8,'http://www.espn.com')
+    player_6 = add_player('Jamaal Charles','RB',8,'http://www.espn.com')
+    player_7 = add_player('Dez Bryant','WR',3,'http://www.espn.com')
+    player_8 = add_player('Julius Thomas','TE',2,'http://www.espn.com')
+    player_9 = add_player('Denver','DEF',3,'http://www.espn.com')
+    player_10 = add_player('Seattle','DEF',4,'http://www.espn.com')
 
 
-def add_player(name,position):
-    c = Player(name=name,position=position)
+def add_player(name,position,bye,url):
+    c = Player(name=name,position=position,bye=bye,url=url)
     db.session.add(c)
     db.session.commit()
 
